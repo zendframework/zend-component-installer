38e821d60c1e22718612724049a05ff8ed031923
Matthew Weier O'Phinney <matthew@zend.com>
2015-12-13 18:00:28 -0600

    create a new remote against which to branch/push

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,11 +6,11 @@ ssh-add .travis/build_key.pem
 curl -LSs https://box-project.github.io/box2/installer.php | php
 php box.phar build -vv
 mv component-installer.phar component-installer.phar.tmp
-git remote add origin git@github.com:weierophinney/component-installer.git
-git fetch origin
-git checkout -b gh-pages origin/gh-pages
+git remote add deploy git@github.com:weierophinney/component-installer.git
+git fetch deploy
+git checkout -b gh-pages deploy/gh-pages
 mv component-installer.phar.tmp component-installer.phar
 sha1sum component-installer.phar > component-installer.phar.version
 git add component-installer.phar component-installer.phar.version
 git commit -m 'Rebuilt phar'
-git push origin gh-pages:gh-pages
+git push deploy gh-pages:gh-pages
