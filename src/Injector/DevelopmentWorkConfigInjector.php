247ef3a7148969c937f1dcefe72cbfc0cadc6578
malinink <apmath@malinink.ru>
2016-07-15 10:03:47 +0300

    add DevelopmentConfigInjector for development.config.php

---
+++ src/Injector/DevelopmentWorkConfigInjector.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies Ltd (http://www.zend.com)
+ */
+
+namespace Zend\ComponentInstaller\Injector;
+
+class DevelopmentWorkConfigInjector extends ApplicationConfigInjector
+{
+    /**
+     * Configuration file to update.
+     *
+     * @var string
+     */
+    protected $configFile = 'config/development.config.php';
+}
