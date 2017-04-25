2808a5709976c385fb41c6f2778131b6e1a881c3
Matthew Weier O'Phinney <matthew@zend.com>
2017-04-25 10:45:25 -0500

    Fixed test autoloader when used under PHPUnit 6
    The legacy class alias for `PHPUnit_Framework_ExpectationFailedException`
    only needs to be performed if the class actually exists, which does not
    happen in PHPUnit 6.

--- test/_autoload.php
+++ test/_autoload.php
@@ -7,4 +7,6 @@
 
 use PHPUnit\Framework\ExpectationFailedException;
 
-class_alias('PHPUnit_Framework_ExpectationFailedException', ExpectationFailedException::class);
+if (class_exists('PHPUnit_Framework_ExpectationFailedException')) {
+    class_alias('PHPUnit_Framework_ExpectationFailedException', ExpectationFailedException::class);
+}
